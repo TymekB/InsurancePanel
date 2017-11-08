8cd97ce131405e2e58b51a00c40c87d67dddfdd0
TymekB <baran.tymoteusz@gmail.com>
2017-11-08 22:13:24 +0100

    added access control

--- app/Http/Controllers/ClientsController.php
+++ app/Http/Controllers/ClientsController.php
@@ -7,6 +7,16 @@ use App\Client;
 
 class ClientsController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function create()
     {
         return view('clients.create');
