0acf21ef52fa4605acd09f2b10993d0c3ee3ed02
TymekB <baran.tymoteusz@gmail.com>
2017-11-08 20:32:35 +0100

    added Client model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,38 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="panel panel-default">
+        <div class="panel-heading">Dashboard</div>
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+        <div class="panel-body">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
                 </div>
-            </div>
+            @endif
+
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Firstname</th>
+                    <th>Lastname</th>
+                    <th>Insurance company</th>
+                    <th>Expiration date</th>
+                    <th>Created at</th>
+                    <th>Last update</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td>20-10-2017</td>
+                    <td>20-09-2017</td>
+                    <td>20-09-2017</td>
+                    <td>20-09-2017</td>
+                </tr>
+                </tbody>
+            </table>
         </div>
     </div>
 </div>
